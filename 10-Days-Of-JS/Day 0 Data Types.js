f5283169cb21abf15cc0ee181cc1d338628b348d
0II <jacklahvh@gmail.com>
2020-05-13 14:48:45 +0100

    🔥

---
+++ 10-Days-Of-JS/Day 0 Data Types.js
@@ -0,0 +1,10 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    secondInteger = parseInt(secondInteger);
+    console.log(firstInteger + secondInteger);
+    const firstDecimal = 4.0;
+    secondDecimal = parseFloat(secondDecimal);
+    console.log(firstInteger + secondDecimal);
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+}
