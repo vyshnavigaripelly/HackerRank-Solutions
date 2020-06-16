f006b3e3c55331dd0b77967ba3e88a75d20f3599
Jack Latimer <jpwww@protonmail.com>
2020-06-16 15:35:35 +0100

    🔥

---
+++ Problem Solving/Migratory Birds.js
@@ -0,0 +1,16 @@
+function migratoryBirds(arr) {
+    let sorted = arr.sort();
+    let ans = sorted[0];
+    let maxOccur = 1,
+        counter = 0;
+    
+    for (let i = 0; i < sorted.length; i++) {
+        counter = (sorted[i] === sorted[i-1]) ? counter + 1 : 0;
+        if (counter > maxOccur) {
+            ans = sorted[i];
+            maxOccur = counter;
+        }
+    }
+
+    return ans;
+}
