090ece63d3ac217126f3fb3b417ef300ede7d6be
Jack Latimer <jpwww@protonmail.com>
2020-06-18 12:18:26 +0100

    🔥

---
+++ Problem Solving/Day of the Programmer.js
@@ -0,0 +1,11 @@
+function dayOfProgrammer(year) {
+    if (year < 1918) {
+      let days = year % 4 === 0 ? 244 : 243;
+      return 256 - days + '.09.' + year;
+    } else if (year > 1918) {
+      let days = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 244 : 243;
+      return 256 - days + '.09.' + year;
+    } else {
+      return '26.09.1918';
+    }
+  }
